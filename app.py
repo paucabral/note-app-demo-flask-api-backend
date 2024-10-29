b9ec6e370c652105af60d991f66c8dcafe68c669
paucabral <josepaucabral.gmail.com>
2024-10-29 09:37:10 +0800

    chore: remove comment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models import db
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all() # Create the DB if not exists
+        db.create_all()
     app.run(
             debug=bool(os.getenv("DEBUG")) if os.getenv("DEBUG") else False,
             port=int(os.getenv("PORT")) if os.getenv("PORT") else 5000,
